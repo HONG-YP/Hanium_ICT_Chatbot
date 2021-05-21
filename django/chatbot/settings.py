e13d1e18c741f53c96a412911a409a9824c5f2fb
HONG_YP <49358347+HONG-YP@users.noreply.github.com>
2021-05-22 00:10:07 +0900

    Update settings.py

--- django/chatbot/settings.py
+++ django/chatbot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'thrg+e+jdbi+dqn1qyf(-v5k@*ety4hsfn+lj4a14&6cd&x4dc'
+SECRET_KEY = '--'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
